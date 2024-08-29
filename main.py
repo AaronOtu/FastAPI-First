fbb5f3c32ccfb937b1a1f1bdd7fa102e149c05a0
Aaron Otu <aaronotu99@gmail.com>
2024-08-29 07:01:48 -0700

    final changes

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
+from blog import models
+from blog.database import engine
+from blog.routers import blog, user, authentication
+
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.get('/blog')
-def index(limit, published: bool = True, sort: Optional[str] = None):
-    if published:
-        return {'data': f'{limit} published blogs from the db'}
-    else:
-
-        return {'data': f'{limit} blogs from the db'}
+@app.get("/")
+async def read_root():
+    return {"message": "App installed successfully"}
 
 
-@app.get('/blog/{id}')
-def show(id: int):
-    return {'data': id}
+app.include_router(authentication.router)
+app.include_router(user.router)
+app.include_router(blog.router)
 
 
-@app.get('/blog/{id}/comments')
-def comments(id):
-    return {'data': {'1', '2'}}
 
 
-class Blog(BaseModel):
-    title: str
-    description: str
-    published: Optional[bool] = None
 
 
-@app.post('/blog')
-def create_blog(blog: Blog):
-    return {'data': f'The title of the blog created is {blog.title}'}
+
+
